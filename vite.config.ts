1d382e7f311c74925d93dcc42815ca98a94aaeef
andrey <the@dgt.dev>
2023-12-26 14:56:29 +0400

    build: add typings to the dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import * as path from "path"
-
 import { defineConfig } from "vite"
+import dts from "vite-plugin-dts"
 
 export default defineConfig({
 	resolve: {
@@ -12,7 +12,11 @@ export default defineConfig({
 		lib: {
 			name: "anything-gpt",
 			entry: "./src/index.ts",
-			formats: ["umd"]
+			formats: ["umd"],
+			fileName: () => "index.js"
 		}
-	}
+	},
+	plugins: [dts({
+		rollupTypes: true
+	})]
 })
