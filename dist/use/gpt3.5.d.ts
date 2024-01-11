818053666449480b6d164854d97c9441bd69c34a
andrey <the@dgt.dev>
2024-01-12 02:17:21 +0400

    build: rebuild

---
+++ dist/use/gpt3.5.d.ts
@@ -0,0 +1,2 @@
+declare const _default: (api_key: string) => (...args: any[]) => Promise<(fn: import("..").StreamNextFn<import("..").ChatMessage[]>) => Promise<import("..").ChatMessage[]>>;
+export default _default;
