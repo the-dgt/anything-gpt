eebb97b057bbeb86c2cce7fe9dcfca67736eea12
andrey <the@dgt.dev>
2024-01-21 13:51:01 +0400

    build: rebuild

--- dist/use/gpt3.5.d.ts
+++ dist/use/gpt3.5.d.ts
@@ -1,2 +1,2 @@
-declare const instance: (api_key: string) => (...args: any[]) => Promise<(fn: import("../types").StreamNextFn<import("../types").ChatMessage[]>) => Promise<import("../types").ChatMessage[]>>;
-export default instance;
+declare const _default: (api_key: string) => (...args: any[]) => Promise<(fn: import("../types").StreamNextFn<import("../types").ChatMessage[]>) => Promise<import("../types").ChatMessage[]>>;
+export default _default;
